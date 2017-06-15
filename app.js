1b03be1e835fcc1e5a082c36a3c5b81a3bfdd6e7
Omar Abou Dehn <3omar.a.de@gmail.com>
2017-06-15 22:27:24 +0200

    created tables and added a pug file

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const pg = require('pg');
+const Sequelize = require('sequelize');
+const db = new Sequelize(`postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@localhost/postgres`); 
+const session = require('express-session');
+
+app.use('/', bodyParser()); //creates key-value pairs request.body in app.post, e.g. request.body.username
+app.use(express.static('src/public'));
+app.use(session({
+	secret: process.env.secret,
+	resave: true,
+	saveUninitialized: false
+}));
+
+// Create model user
+var User = db.define('user', {
+	name: {type: Sequelize.STRING, allowNull: false},
+	email: {type: Sequelize.STRING, allowNull: false, uniqe: true},
+	password: {type: Sequelize.STRING, allowNull: false}
+})
+
+// Create model roster
+var Roster = db.define('roster', {
+	name: {type: Sequelize.STRING, allowNull: false},
+	mon: {type: Sequelize.STRING, allowNull: false},
+	tues: {type: Sequelize.STRING, allowNull: false},
+	wed: {type: Sequelize.STRING, allowNull: false},
+	thurs: {type: Sequelize.STRING, allowNull: false},
+	fri: {type: Sequelize.STRING, allowNull: false}
+})
+
+
+
+db.sync({force: true});
+
+
+const listener = app.listen(3000, function () {
+	console.log('Example app listening on port: ' + listener.address().port);
+});
